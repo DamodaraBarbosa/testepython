c8cbea66b354a4c7976f02ab5dde78ceb52952cf
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-06-13 10:35:09 -0300

    Adição da função tabela

--- funções.py
+++ funções.py
@@ -1,2 +1,7 @@
 def erro():
     print('\033[1;31mValor inválido, digite novamente!\033[m')
+
+def tabela ():
+    print('\033[1;33m--\033[m'*30)
+    print('\033[1;33mDADOS DOS PRODUTOS\033[m')
+    print('\033[1;33m--\033[m'*30)
