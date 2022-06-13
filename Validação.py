243aa896dbedab040682cdc4c3bec7ec1f75143d
DamodaraBarbosa <damodarabarbosa@gmail.com>
2022-06-13 10:05:37 -0300

    Alteração

---
+++ Validação.py
@@ -0,0 +1,48 @@
+import funções
+
+
+produtos = list()
+dados = list()
+
+while True:
+    nome = str(input('Nome do produto: '))
+    vnome = nome.replace(" ", "")
+    while vnome.isalpha() == False:
+        funções.erro()
+        nome = str(input('Nome do produto: '))
+        vnome = nome.replace(" ", "")
+    dados.append(nome)
+    try:
+        preço = float(input('Preço do produto: R$ '))
+    except ValueError:
+        funções.erro()
+        while True:
+            try:
+                preço = float(input('Preço do produto: R$ '))
+                break
+            except ValueError:
+                funções.erro()
+    dados.append(preço)
+    try:
+        estoque = int(input('Quantidade no estoque: '))
+    except ValueError:
+        funções.erro()
+        while True:
+            try:
+                estoque = int(input('Quantidade no estoque: '))
+                break
+            except ValueError:
+                funções.erro()
+    dados.append(estoque)
+    produtos.append(dados[:])
+    produtos.clear()
+    continuidade = str(input('Deseja continuar [S/N]? ')).upper().strip()
+    while continuidade not in 'SN':
+        funções.erro()
+        continuidade = str(input('Deseja continuar [S/N]? ')).upper().strip()
+    if continuidade == 'N':
+        break
+print('--'*30)
+print(dados)
+print('--'*30)
+
---
+++ funções.py
@@ -0,0 +1,2 @@
+def erro():
+    print('\033[1;31mValor inválido, digite novamente!\033[m')
